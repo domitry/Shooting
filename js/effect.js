bf4cac98bbe4d4670322d3962de9f330e7021525
Naoki Nishida <domitry@gmail.com>
2015-09-01 05:14:52 +0900

    Add effect system

---
+++ js/effect.js
@@ -0,0 +1,38 @@
+/*global require, module, $, jQuery*/
+
+module.exports = {
+    init: function(obj_manager){
+        this.obj_manager = obj_manager;
+    },
+
+    explode: function(x, y, _options){
+        var options = $.extend({
+            speed: 10,
+            r_max: 15,
+            num: 10,
+            char: "x",
+            time: 70
+        }, _options);
+        
+        for(var i=0; i<options.num; i++){
+            //var size = parseInt(options.r_max * Math.random());
+            var size = options.r_max;
+            var speed = options.speed*Math.random();
+            var theta = 2*Math.PI*Math.random();
+            var dx = speed*Math.cos(theta);
+            var dy = speed*Math.sin(theta);
+            var obj = this.obj_manager.add("others", options.char, x, y, dx, dy, {
+                size: size
+            });
+            
+            obj.cnt=0;
+            var f = obj.update;
+            obj.update = function(){
+                f.apply(this);
+                obj.cnt++;
+                if(obj.cnt < options.time)return true;
+                else return false;
+            };
+        }
+    }
+};
